6c0e8d31fad0ac7d15cc662690d637573e6713f5
Caldwel <caldwelveshesh@gmail.com>
2026-09-09 10:21:02 +0530

    Complete practicum: contacts CRUD app with HubSpot API integration

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,20 +9,70 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS =process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+  const searchEndpoint = 'https://api.hubapi.com/crm/v3/objects/contacts/search';
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
+  const body = {
+    properties: ['full_name', 'favorite_color', 'bio'],
+    limit: 100,
+    sorts: [
+      {
+        propertyName: 'createdate',
+        direction: 'DESCENDING'
+      }
+    ]
+  };
+
+  try {
+    const resp = await axios.post(searchEndpoint, body, { headers });
+    const contacts = resp.data.results;
+    res.render('homepage', { title: 'Contacts List', contacts });
+  } catch (error) {
+    console.error(error);
+    res.send('Error fetching contacts');
+  }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', (req, res) => {
+  res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+  const createContact = 'https://api.hubapi.com/crm/v3/objects/contacts';
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
+  const contactData = {
+    properties: {
+      full_name: req.body.full_name,
+      favorite_color: req.body.favorite_color,
+      bio: req.body.bio
+    }
+  };
 
+  try {
+    await axios.post(createContact, contactData, { headers });
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    res.send('Error creating contact');
+  }
+});
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
 
